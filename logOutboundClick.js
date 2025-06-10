adb64914ca8329ab004dd6a17968b51c58253737
Can Özden <ademcanozden@gmail.com>
2025-06-11 00:28:21 +0300

    add outbound click script to send analytics data - gbraid is added for iOS +14 add tracking measurements

---
+++ logOutboundClick.js
@@ -0,0 +1,20 @@
+<script>
+(function() {
+    var endpoint = "https://rg6u2gsl87.execute-api.eu-central-1.amazonaws.com/outbound-click-logs"; 
+    var params = new URLSearchParams(window.location.search);
+    var gclid = params.get("gclid");
+    var gbraid = params.get("gbraid");
+
+    if (!gclid && !gbraid) return;
+
+
+    var payload = JSON.stringify({
+        gclid: gclid,
+        gbraid: gbraid,
+        pagePath: window.location.pathname
+    });
+    var blob = new Blob([payload], {type: "text/plain"});
+
+    navigator.sendBeacon(endpoint, blob);
+})();
+</script>
