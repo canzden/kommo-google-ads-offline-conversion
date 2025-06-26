660584378c3389d7a8411b5d0f6698f89cfef3a2
Can Özden <ademcanozden@gmail.com>
2025-06-27 00:48:16 +0300

    add retrieval of cookies as fallback * tracking params removed when user navigates, so cookies should be retrieved if params are not present

--- logOutboundClick.js
+++ logOutboundClick.js
@@ -1,15 +1,27 @@
 <script>
 (function() {
+    function getCookies(trackingCookieNames){
+        var cookies = document.cookie 
+            .split('; ')
+            .map(pair => pair.split('='))
+            .map(([key, val]) => [decodeURIComponent(key), decodeURIComponent(val)])
+            .filter(([key]) => trackingCookieNames.includes(key));
+        return Object.fromEntries(cookies)
+    }
+
     var endpoint = "https:/YOUR_LAMBDA_API_GATEWAY_URL/outbound-click-logs"; 
+
     var params = new URLSearchParams(window.location.search);
     var gclid = params.get("gclid");
     var gbraid = params.get("gbraid");
+    
+    var cookies = getCookies(['_ads_gclid', '_ads_gbraid'])
 
-    if (!gclid && !gbraid) return;
+    if ((!gclid && !gbraid) || Object.keys(cookies).length == 0) return;
 
     var payload = JSON.stringify({
-        gclid: gclid,
-        gbraid: gbraid,
+        gclid: gclid || cookies['_ads_gclid'],
+        gbraid: gbraid || cookies['_ads_gbraid'],
         pagePath: window.location.pathname
     });
     var blob = new Blob([payload], {type: "text/plain"});
